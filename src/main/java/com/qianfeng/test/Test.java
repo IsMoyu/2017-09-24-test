dccd50705d85bbae23a68009f5c61ae6a64b2b90
870685657@ <Fjk2013>
2017-09-24 21:58:00 +0800

    tijiao

--- src/main/java/com/qianfeng/test/Test.java
+++ src/main/java/com/qianfeng/test/Test.java
@@ -5,7 +5,17 @@ package com.qianfeng.test;
  */
 public class Test {
     public static void main(String[] args) {
+
         int x=9;
         System.out.println("上传");
+    }
+
+    public void print(){
+        System.out.println("hh");
+        System.out.println(11);
+    }
+    public void print1(){
+        System.out.println("hh");
+        System.out.println(11);
     }
 }
